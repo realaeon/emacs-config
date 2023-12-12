2bf3ba9944f331df8007b26552d595466601f0c9
liweiwei.42 <liweiwei.42@bytedance.com>
2023-12-12 21:44:54 +0800

    fix: after org and dashboard

--- config.el
+++ config.el
@@ -40,62 +40,68 @@
 
 ;; my system config
 (setq system-time-locale "C")
+(setq fancy-splash-image (concat doom-user-dir "splash.svg"))
+(setq +doom-dashboard-functions
+      (list #'doom-dashboard-widget-banner
+            #'doom-dashboard-widget-shortmenu
+            #'doom-dashboard-widget-loaded))
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-;;(after! org
-(setq org-log-into-drawer t)
-(setq org-todo-keywords
-      '((sequence
-         "TODO(t)"  ; A task that needs doing & is ready to do
-         "PROJ(p)"  ; A project, which usually contains other tasks
-         "LOOP(r)"  ; A recurring task
-         "STRT(s@)"  ; A task that is in progress
-         "WAIT(w@)"  ; Something external is holding up this task
-         "NEXT(n@)"
-         "HOLD(h@)"  ; This task is paused/on hold because of me
-         "IDEA(i@)"  ; An unconfirmed and unapproved task or notion
-         "|"
-         "DONE(d@)"  ; Task successfully completed
-         "KILL(k@)") ; Task was cancelled, aborted, or is no longer applicable
-        (sequence
-         "[ ](T)"   ; A task that needs doing
-         "[-](S@)"   ; Task is in progress
-         "[?](W@)"   ; Task is being held up or paused
-         "|"
-         "[X](D@)")  ; Task was completed
-        (sequence
-         "|"
-         "OKAY(O)"
-         "YES(Y)"
-         "NO(N)"))
-      org-todo-keyword-faces
-      '(("[-]"  . +org-todo-active)
-        ("STRT" . +org-todo-active)
-        ("NEXT" . +org-todo-active)
-        ("[?]"  . +org-todo-onhold)
-        ("WAIT" . +org-todo-onhold)
-        ("HOLD" . +org-todo-onhold)
-        ("PROJ" . +org-todo-project)
-        ("NO"   . +org-todo-cancel)
-        ("KILL" . +org-todo-cancel)))
-;; with fast selection keys
-(srrq org-tag-alist (quote ((:startgroup)
-                            ("@HOME" . ?h)
-                            ("@WORK" . ?w)
-                            ("@RESEARCH" . ?r)
-                            (:endgroup)
-			    ("DL" . ?P)
-                            ("HAND" . ?H)
-                            ("CODE" . ?c))))
-(setq org-agenda-custom-commands
-      '(("w" tags-todo "+@WORK")
-	("h" tags-todo "+@HOME")
-        ("r" tags-todo "+@RESEARCH")
-	))
-;;)
+
+(after! org-agenda
+  (setq org-log-into-drawer t)
+  (setq org-todo-keywords
+        '((sequence
+           "TODO(t)"  ; A task that needs doing & is ready to do
+           "PROJ(p)"  ; A project, which usually contains other tasks
+           "LOOP(r)"  ; A recurring task
+           "STRT(s@)"  ; A task that is in progress
+           "WAIT(w@)"  ; Something external is holding up this task
+           "NEXT(n@)"
+           "HOLD(h@)"  ; This task is paused/on hold because of me
+           "IDEA(i@)"  ; An unconfirmed and unapproved task or notion
+           "|"
+           "DONE(d@)"  ; Task successfully completed
+           "KILL(k@)") ; Task was cancelled, aborted, or is no longer applicable
+          (sequence
+           "[ ](T)"   ; A task that needs doing
+           "[-](S@)"   ; Task is in progress
+           "[?](W@)"   ; Task is being held up or paused
+           "|"
+           "[X](D@)")  ; Task was completed
+          (sequence
+           "|"
+           "OKAY(O)"
+           "YES(Y)"
+           "NO(N)"))
+        org-todo-keyword-faces
+        '(("[-]"  . +org-todo-active)
+          ("STRT" . +org-todo-active)
+          ("NEXT" . +org-todo-active)
+          ("[?]"  . +org-todo-onhold)
+          ("WAIT" . +org-todo-onhold)
+          ("HOLD" . +org-todo-onhold)
+          ("PROJ" . +org-todo-project)
+          ("NO"   . +org-todo-cancel)
+          ("KILL" . +org-todo-cancel)))
+  ;; with fast selection keys
+  (setq org-tag-alist (quote ((:startgroup)
+                              ("@HOME" . ?h)
+                              ("@WORK" . ?w)
+                              ("@RESEARCH" . ?r)
+                              (:endgroup)
+			      ("DL" . ?P)
+                              ("HAND" . ?H)
+                              ("CODE" . ?c))))
+  (setq org-agenda-custom-commands
+        '(("w" tags-todo "+@WORK")
+	  ("h" tags-todo "+@HOME")
+          ("r" tags-todo "+@RESEARCH")
+	  ))
+  )
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
